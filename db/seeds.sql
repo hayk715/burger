2c45cfc11cf8977570657409633bae5a91d29d7c
hayk715 <hayk715@gmail.com>
2018-09-14 16:01:44 -0700

    make it better

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name) VALUES ('CheeseBurger');
+INSERT INTO burgers (burger_name) VALUES ('Beef Burger');
+INSERT INTO burgers (burger_name) VALUES ('Veggie Burger');
+INSERT INTO burgers (burger_name) VALUES ('Chicken Burger');
+
