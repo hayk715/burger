8fdfb838f4663ee21875421411cbaa5dd15283e9
hayk715 <hayk715@gmail.com>
2018-09-14 16:18:10 -0700

    change

--- server.js
+++ server.js
@@ -20,5 +20,6 @@ var routes = require("./controllers/burgers_controller.js");
 
 app.use("/", routes);
 
-app.listen(port);
-
+app.listen(PORT, function () {
+    console.log('App listening on PORT: ' + PORT);
+  });
