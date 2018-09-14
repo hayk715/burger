86f09b772ad693d2cc091946317c59de27da7472
hayk715 <hayk715@gmail.com>
2018-09-14 16:08:10 -0700

    better

--- config/connection.js
+++ config/connection.js
@@ -11,7 +11,7 @@ else {
         host: "localhost",
         port: 3306,    
         user: "root",
-        password: "bronco123",
+        password: "",
         database: "burgers_db"
     });
 }
