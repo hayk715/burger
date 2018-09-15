955752b51ea41fa8928f243fd17dd05560672706
hayk715 <hayk715@gmail.com>
2018-09-15 12:37:35 -0700

    change

--- server.js
+++ server.js
@@ -18,8 +18,10 @@ app.set("view engine", "handlebars");
 
 // Import routes and give the server access to them.
 var routes = require("./controllers/burgers_controller.js");
+//Import routes and give server access to them.
+app.use(routes);
 
-app.use("/", routes);
-
-app.listen(port);
-
+//Start server so it can begin listening to client requests. 
+app.listen(PORT, function() {
+    console.log("Server listening on: http://localhost:" + PORT);
+});
